95cb820170c014cceca5bcf8622d3018200bb24c
HoangKien <HoangKien>
2022-02-19 20:02:50 +0700

    HoangKien

---
+++ Exam/Data/DatabaseInitialize.cs
@@ -0,0 +1,60 @@
+﻿using Exam.Entity;
+using SQLitePCL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exam.Data
+{
+    class DatabaseInitialize
+    {
+        public static bool CreateTables()
+        {
+            var conn = new SQLiteConnection("sqliteContact.db");
+            string sql = @"CREATE TABLE IF NOT EXISTS
+                        Contact (
+                        Id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+                        Name VARCHAR( 140 ),
+                        Phone VARCHAR(10) UNIQUE
+                        );";
+            using (var statement = conn.Prepare(sql))
+            {
+                statement.Step();
+            }
+            return true;
+        }
+
+        public static bool SaveTables(Contact contact)
+        {
+            var conn = new SQLiteConnection("sqliteContact.db");
+            using (var contactSql = conn.Prepare("INSERT INTO Contact(Name, Phone) VALUES (?, ?)"))
+            {
+                contactSql.Bind(1, contact.Name);
+                contactSql.Bind(2, contact.Phone);
+                contactSql.Step();
+            }
+            return true;
+        }
+
+        public Contact GetContact(string name)
+        {
+            Contact contact = null;
+            var conn = new SQLiteConnection("sqliteContact.db");
+            using (var statement = conn.Prepare("SELECT Name, Phone FROM  Contact WHERE Name = ? "))
+            {
+                statement.Bind(1, name);
+                if (statement.Step() == SQLiteResult.ROW)
+                {
+                    contact = new Contact()
+                    {
+                     Name = (string)statement["Name"],
+                     Phone = (string)statement["Phone"]
+                    };
+                }
+            }
+            return contact;
+        }
+    }
+}
---
+++ Exam/MainPage.xaml.cs
@@ -0,0 +1,67 @@
+﻿using Exam.Data;
+using Exam.Entity;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Exam
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Contact contact = new Contact()
+            {
+                Name = txtName.Text,
+                Phone = txtPhone.Text
+            };
+            ContentDialog contentDialog = new ContentDialog();
+            if (Data.DatabaseInitialize.SaveTables(contact)){
+                contentDialog.Title = "Action success !!";
+                contentDialog.Content = "Save success !!";
+            }
+            else
+            {
+                contentDialog.Title = "Action Failed !!";
+                contentDialog.Content = "Save Failed !!";
+            }
+            contentDialog.CloseButtonText = "Okie";
+            await contentDialog.ShowAsync();
+        }
+
+        private void Search(object sender, RoutedEventArgs e)
+        {
+            DatabaseInitialize data = new DatabaseInitialize();
+            Contact contact = data.GetContact(txtSearch.Text);
+            if(contact == null)
+            {
+                result.Text = "Contact not found";
+            }
+            else
+            {
+                result.Text = contact.ToString();
+            }
+        }
+    }
+}
